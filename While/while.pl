f3286a9e3e61304ef4aa123fd0261020064d030b
John Gallagher <john@gallagher.dk>
2025-06-20 13:39:13 +0200

    Added observation points.

--- While/while.pl
+++ While/while.pl
@@ -2,7 +2,6 @@
 
 :- use_module(while_reg).
 :- use_module(transformExpr).
-:- use_module(library(random)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 % initial setup and entry
@@ -18,40 +17,33 @@ go(File,Style,LR,YN) :-
     read(S,P),
     close(S),
     P=logen(program/1,program(Prog)),
-    vardecls(Prog,St),				% Top level variable declarations
+    %vardecls(Prog,St),				% Top level variable declarations
 	member(function(main,[],Code),Prog),
-    regexp(Code,Expr),
-    exec(Expr,Style,LR,YN,St).
+	genvardecls(Prog,Code1,Code),
+    regexp(let(var(cost),cns(nat(0)),Code1),Expr),	% Initialise cost variable
+    exec(Expr,Style,LR,YN,[]).
     
 exec(Expr,big,right,no,St) :-
 	copyStateSkeleton(St,St1),
-	bigstep(Expr,right,St,St1),
-	write(St1),
-	nl.
+	bigstep(Expr,right,St,St1).
 exec(Expr,small,right,no,St) :-	
 	run(Expr,St).
 exec(Expr,big,left,no,St) :-
 	copyStateSkeleton(St,St1),
-	bigstep(Expr,left,St,St1),
-	write(St1),
-	nl.
+	bigstep(Expr,left,St,St1).
 exec(Expr,small,left,no,St) :-
 	run(Expr,St).
 exec(Expr,big,right,yes,St) :-
 	transformRegExpr(Expr,TExpr),
 	copyStateSkeleton(St,St1),
-	bigstep(TExpr,right,St,St1),
-	write(St1),
-	nl.
+	bigstep(TExpr,right,St,St1).
 exec(Expr,small,right,yes,St) :-
 	transformRegExpr(Expr,TExpr),
 	run(TExpr,St).
 exec(Expr,big,left,yes,St) :-
 	transformRegExpr(Expr,TExpr),
 	copyStateSkeleton(St,St1),
-	bigstep(TExpr,left,St,St1),
-	write(St1),
-	nl.
+	bigstep(TExpr,left,St,St1).
 exec(Expr,small,left,yes,St) :-
 	transformRegExpr(Expr,TExpr),
 	run(TExpr,St).		
@@ -64,6 +56,13 @@ vardecls([[vardecl(var(X),_,Init)]|P],[(X,V)|St]) :-
 vardecls([T|P],St) :-
 	T \= [vardecl(_,_,_)],
 	vardecls(P,St).
+	
+genvardecls([],Code0,Code0).
+genvardecls([[vardecl(var(X),_,Init)]|P],let(var(X),Init,Code1),Code0) :-
+	genvardecls(P,Code1,Code0).
+genvardecls([T|P],Code1,Code0) :-
+	T \= [vardecl(_,_,_)],
+	genvardecls(P,Code1,Code0).
 	
 initDeclVal(cns(nat(V)),V).
 initDeclVal(null,_).
@@ -86,8 +85,9 @@ initDeclVal(null,_).
 % Big-step interpretation wrt a regular expression and left/right recursion
 
 bigstep(eps,_,St,St).
-bigstep(asg(var(X),E),_,St0,St1) :-
-	evalAndSave(X,E,St0,St1).
+bigstep(asg(var(X),E),_,St0,St2) :-
+	evalAndSave(X,E,St0,St1),
+	evalAndSave(cost,add(var(cost),cns(nat(1))),St1,St2).	% increment cost
 bigstep(true(E),_,St,St) :-
 	evaltrue(E,St).
 bigstep(false(E),_,St,St) :-
@@ -96,6 +96,7 @@ bigstep(decl(var(X),E):(E1:release(var(X))),LR,St0,St3) :-
 	evalAndSave(X,E,[(X,_)|St0],St1),
 	copyStateSkeleton(St1,St2),
 	bigstep(E1,LR,St1,St2),
+	observeStates(St1,St2),
 	removeVar(X,St2,St3).
 bigstep(E1:E2,LR,St0,St2) :-
 	functor(E1,F,N),
@@ -117,14 +118,14 @@ bigstep(star(E),left,St0,St1) :-		% Left recursive interpretation
 % Small-step interpretation wrt a regular expression 
 % Ignore left/right with small-step - unclear what it means
 
-run(eps,St) :-
-	haltState(St).
+run(eps,_St).
 run(Expr,St) :-
 	step(Expr,Expr1,St,St1),
 	run(Expr1,St1).
 	
-step(asg(var(X),E),eps,St0,St1) :-
-	evalAndSave(X,E,St0,St1).
+step(asg(var(X),E),eps,St0,St2) :-
+	evalAndSave(X,E,St0,St1),
+	evalAndSave(cost,add(var(cost),cns(nat(1))),St1,St2).	% increment cost
 step(true(E),eps,St0,St0) :-
 	evaltrue(E,St0).
 step(false(E),eps,St0,St0) :-
@@ -132,6 +133,7 @@ step(false(E),eps,St0,St0) :-
 step(decl(var(X),E),eps,St0,St1) :-
 	evalAndSave(X,E,St0,St1).
 step(release(var(X)),eps,St0,St1) :-
+	observeState(St0),
 	removeVar(X,St0,St1).
 step((eps:S),S,St0,St0).
 step((S1:S2),S11:S2,St0,St1) :-
@@ -146,16 +148,12 @@ step(star(Expr),Expr1:star(Expr),St0,St1) :-		% Right recursive interpretation
 	step(Expr,Expr1,St0,St1).
 
 	
-haltState(St) :-
-	write(St),
-	nl.
-	
 	
 %%%%%%%%%%%%%%%%%%%%%%%%
 %----- expressions -----
 %%%%%%%%%%%%%%%%%%%%%%%%
 
-eval(null,_St,_V).
+eval(null,_St,_V).	% null gives undefined value
 eval(var(X),St,V) :-
 	find(St,X,V).
 eval(cns(nat(N)),_,N).
@@ -195,16 +193,21 @@ eval(E1==E2,St0,V) :-
 	eval(E1,St0,V1),
 	eval(E2,St0,V2),
 	eq(V1,V2,V).
-eval(logicaland(E1,E2),St0,V) :-
-	eval(E1,St0,V1),
-	eval(E2,St0,V2),
-	logicalAnd(V,V1,V2).
+eval(logicaland(E1,E2),St0,1) :-
+	eval(E1,St0,1),
+	eval(E2,St0,1).
+eval(logicaland(E1,_E2),St0,0) :-
+	eval(E1,St0,0).
+eval(logicaland(_E1,E2),St0,0) :-
+	eval(E2,St0,0).
 eval(not(E),St0,V) :-
 	eval(E,St0,V1),
 	negate(V1,V).
-eval(call(rand,[]),_St0,V) :-	% ad hoc handling of rand() function as nondet boolean
-	random(V1),
-	V is round(V1).
+eval(call(rand,[]),_St0,V) :-	% ad hoc handling of rand() function
+	%random(V1),
+	%V is round(V1).
+	V>=0,
+	V=<1.
 	
 
 evaltrue(E,St) :-
@@ -227,12 +230,12 @@ find([(Y,_)|St],X,N) :-
 	find(St,X,N).
 	
 save(X,V,[(X,_)|St],[(X,W)|St]) :-
-	W is V.
+	W = V.
 save(X,V,[(Y,M)|St],[(Y,M)|St1]) :-
 	X \== Y,
 	save(X,V,St,St1).
 save(X,V,[],[(X,W)]) :-
-	W is V.
+	W = V.
 	
 removeVar(X,[(X,_)|St],St).
 removeVar(X,[(Y,M)|St],[(Y,M)|St1]) :-
@@ -263,11 +266,7 @@ eq(X,Y,1) :-
 	X == Y.
 eq(X,Y,0) :-
 	X \== Y.
-	
-logicalAnd(1,V1,V2) :-
-	1 is V1/\V2.
-logicalAnd(0,V1,V2) :-
-	0 is V1/\V2.			
+				
 	
 negate(1,0).
 negate(0,1).
@@ -275,6 +274,13 @@ negate(0,1).
 copyStateSkeleton([],[]).
 copyStateSkeleton([(X,_)|St],[(X,_)|St1]) :-
 	copyStateSkeleton(St,St1).
+	
+observeState(St) :-	
+	write(St),
+	nl.
+observeStates(St0,St1) :-	
+	write((St0,St1)),
+	nl.
 
 % Getting and setting options
 
